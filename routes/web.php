8e8fdd5e1d5175e84f38cd8bdc59355c22e64e88
cahyasifa <cahyasifanazwa07@gmail.com>
2025-03-26 12:51:34 +0700

    Menambahkan view lapangan dan pelanggan, cahya sifa

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,42 @@
 <?php
 
-use App\Http\Controllers\DashboardController
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+Route::get('/welcome', function () {
+    return view('welcome');
+});
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard'); 
-    
+Route::get('/user/{id}' , function ($id) {
+    return 'User dengan ID' . $id;
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return 'Admin Dashboard';
+        });
+
+        Route::get('/users', function () {
+            return 'Admin Users';
+            });
+});
+
+Route::get('/listbarang/{id}/{nama}', function ($id, $nama) {
+    return view('list_barang', compact('id', 'nama'));
+});
+
+// Route::get('/listbarang/{id}/{nama}', function ($id, $nama) {
+// return view('list_barang', compact('id', 'nama'));
+// });
+
+
+Route::get('/dashboard', function() {
+    return 'Admin Dashboard';
+});
+
+Route::get('/lapangan', function () {
+ return view('lapangan');
+});
+
+Route::get('pelanggan', function() {
+    return view('pelanggan');
+});
