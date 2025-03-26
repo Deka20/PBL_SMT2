f85bab5b89185e00fff315e53ddc7bcbffb83e1a
Deka20 <masadita20@gmail.com>
2025-03-26 13:09:40 +0700

    Memperbaiki Routes

--- routes/web.php
+++ routes/web.php
@@ -1,45 +1,26 @@
 <?php
-
-<<<<<<< HEAD
-//use App\Http\Controllers\HomeController;
-//use App\Http\Controllers\ListBarangController;
-//use Illuminate\Support\Facades\Route;
-//use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
-=======
-use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ListItemController;
 use App\Http\Controllers\DashboardController;
+
 Route::get('/welcome', function () {
     return view('welcome');
 });
 
-Route::get('/user/{id}' , function ($id) {
-    return 'User dengan ID' . $id;
-});
+// Route::prefix('admin')->group(function () {
+//     Route::get('/dashboard', function () {
+//         return 'Admin Dashboard';
+//         });
 
-Route::prefix('admin')->group(function () {
-    Route::get('/dashboard', function () {
-        return 'Admin Dashboard';
-        });
-
-        Route::get('/users', function () {
-            return 'Admin Users';
-            });
-});
+//         Route::get('/users', function () {
+//             return 'Admin Users';
+//             });
+// });
 
 Route::get('/listbarang/{id}/{nama}', function ($id, $nama) {
     return view('list_barang', compact('id', 'nama'));
-});
-
-// Route::get('/listbarang/{id}/{nama}', function ($id, $nama) {
-// return view('list_barang', compact('id', 'nama'));
-// });
-
-
-Route::get('/dashboard', function() {
-    return 'Admin Dashboard';
 });
 
 Route::get('/lapangan', function () {
@@ -48,75 +29,24 @@ Route::get('/lapangan', function () {
 
 Route::get('pelanggan', function() {
     return view('pelanggan');
-=======
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\ListItemController;
-use App\Http\Controllers\DashboardController;
->>>>>>> 8badcec86ddcc238fb1eebffdf7f5b441be8c22d
+});
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-//Route::get('/', [HomeController::class, 'index']);
-//Route::get('contact', [HomeController::class, 'contact']);
-
-//Route::get('/welcome', function () {
-    //return view('welcome');
-//});
-
-//Route::get('user/{id}', function ($id) {
-    //return 'User dengan ID' . $id;
-//});
-
-//Route::prefix('admin')->group(function () {
-    //Route::get('/dashboard', function () {
-        //return 'Admin Dashboard';
-    //});
-
-    //Route::get('/users', function () {
-        //return 'Admin Users';
-    //});
-//});
-
-//Route::get('/listbarang/{id}/{nama}', [ListBarangController::class, 'tampilkan']);
-
-//Route::get('/login', [LoginController::class, 'index']);
-
-//Route::get('/login', function () {
-    //return view('auth.login'); // Sesuaikan dengan nama view login-mu
-//});
 Route::get('/dashboard', function () {
     return view('admin.dashboard');
 });
-<<<<<<< HEAD
+
 //Route::get('/dashboard', [AdminController::class, 'dashboard']);
 Route::get('/LandingPage', function () {
     return view('LandingPage');
-=======
+});
 
 Route::get('user/{id}', function ($id) {
     return 'User dengan ID' . $id;
 });
 
-// Route::prefix('admin')->group(function () {
-//     Route::get('/dashboard', function () {
-//         return 'Admin Dashboard';
-//     });
-
-//     Route::get('/users', function () {
-//         return 'Admin Users';
-//     });
-// });
-
 Route::get('/listitem/{id}/{tipe}', [ListItemController::class, 'tampilkan']);
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
-
-// Route::post('/login', [LoginController::class, 'authenticate'])->name('login.authenticate');
-
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
 
 Route::get('/profile', function () {
     return view('profile');
@@ -132,5 +62,4 @@ Route::get('/login', function () {
 
 Route::get('/register', function () {
     return view('registerPage');
->>>>>>> 8badcec86ddcc238fb1eebffdf7f5b441be8c22d
 });
